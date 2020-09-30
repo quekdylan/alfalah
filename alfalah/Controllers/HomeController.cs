6a89b708049aca002c460a1fec2992e4c4bfb678
Zhi En Dylan Quek <s10186935@connect.np.edu.sg>
2020-09-30 23:43:47 +0800

    Add project files.

---
+++ alfalah/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+
+namespace alfalah.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/about-us")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("/shop")]
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+    }
+}
